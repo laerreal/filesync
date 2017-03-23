88dcbd016cf2af9cd7cec56050b3af2decdcd46a
Vasily Efimov <real@ispras.ru>
2017-03-23 20:26:48 +0300

    filesync: decorator for stateful object classes

--- filesync.py
+++ filesync.py
@@ -198,6 +198,26 @@ class EventContext(object):
             ls = allLs[e]
             ls.remove(cb)
 
+# Decorator for stateful object classes
+class Stateful():
+    def __init__(self, *attrs):
+        self.attrs = attrs
+
+    def __call__(self, klass):
+        def get_state(obj):
+            return obj.__state
+
+        def set_state(obj, state, attrs = self.attrs):
+            for attr in attrs:
+                try:
+                    val = getattr(obj, attr + state)
+                except AttributeError:
+                    val = None
+                setattr(obj, attr, val)
+
+        klass.state = property(get_state, set_state)
+        return klass
+
 # File system model
 # -----------------
 
