5695d6a76bbdb56c663c70ae741d193b6c00d17c
Vasily Efimov <laer.18@gmail.com>
2018-01-04 19:35:14 +0300

    widgets: `Treeview` variant for coroutine dispatcher
    Signed-off-by: Vasily Efimov <laer.18@gmail.com>

---
+++ widgets/coview.py
@@ -0,0 +1,128 @@
+__all__ = [
+    "CoView"
+]
+
+from six.moves.tkinter.ttk import Treeview
+
+def coiid(co):
+    return "co%u" % id(co)
+
+socketRows = {"s2r", "s2w"}
+
+def siid(sock):
+    return "sock%u" % sock.fileno()
+
+class CoView(Treeview):
+    def __init__(self, coDisp = None, **kw):
+        Treeview.__init__(self, **kw)
+
+        self.heading("#0", text = "Task")
+
+        self.insert("", "end",
+            iid = "s2r",
+            text = "Socket readers"
+        )
+        self.insert("", "end",
+            iid = "s2w",
+            text = "Socket writers"
+        )
+
+        self.d = coDisp
+
+    def updateTree(self):
+        d = self.d # dispatcher
+        desc = d.desc # descriptions
+        refs = d.references
+        s2r = d.socketsToRead
+        s2w = d.socketsToWrite
+
+        stack = [("", d.queue + d.ready + d.waiting)]
+
+        toDel = set()
+
+        for sockName in ["s2r", "s2w"]:
+            sockDeps = locals()[sockName]
+
+            children = set(self.get_children(sockName))
+
+            for sock, co in sockDeps.items():
+                iid = siid(sock)
+
+                try:
+                    children.remove(iid)
+                except KeyError:
+                    if self.exists(iid):
+                        self.move(iid, sockName, "end")
+                    else:
+                        # no row for that socket yet
+                        try:
+                            peer = sock.getpeername()
+                        except OSError:
+                            text = "%s (%s)" % (
+                                sock.getsockname(),
+                                sock.fileno()
+                            )
+                        else:
+                            text = "%s (%s) >=< %s" % (
+                                sock.getsockname(),
+                                sock.fileno(),
+                                peer
+                            )
+                        # XXX: There is a negligible chance a socket was
+                        # closed and a new one was opened with same number.
+                        # Then text message should be updated
+
+                        self.insert(sockName, "end",
+                            iid = iid,
+                            text = text
+                        )
+
+                        self.item(iid, open = True)
+
+                stack.append((iid, (co,)))
+
+            toDel |= children
+
+        while stack:
+            parent, allCo = stack.pop()
+            children = set(self.get_children(parent))
+
+            for co in allCo:
+                iid = coiid(co)
+
+                try:
+                    children.remove(iid)
+                except KeyError:
+                    # no row for that task yet
+                    try:
+                        text = desc[co]
+                    except KeyError:
+                        text = co.__name__
+
+                    # ensures that socket rows are always to the bottom
+                    idx = "end" if parent else -2
+
+                    if self.exists(iid):
+                        # a row for that task is somewhere, use it
+                        # print(parent, "<-", iid)
+                        self.move(iid, parent, idx)
+                        # print("ok")
+
+                        if iid in toDel:
+                            toDel.remove(iid)
+                    else:
+                        # print("ins", parent, "<-", iid)
+                        self.insert(parent, idx, iid = iid, text = text)
+                        self.item(iid, open = True)
+                        # print("ok")
+
+                if co in refs:
+                    stack.append((iid, refs[co]))
+
+            toDel |= children
+
+        toDel -= socketRows
+
+        # remove rows of forgotten tasks
+        if toDel:
+            self.delete(*list(toDel))
