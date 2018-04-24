64cd8af6c8ce6fdaa1c98870c6cd090369d7f314
Vasily Efimov <laer.18@gmail.com>
2018-04-25 00:11:18 +0300

    common.raw: Py version independent byte to int conversion helper
    Signed-off-by: Vasily Efimov <laer.18@gmail.com>

--- common/raw.py
+++ common/raw.py
@@ -1,7 +1,16 @@
 __all__ = [
+    "byte2int",
     "bytes2int",
     "int2bytes"
 ]
+
+from sys import version_info
+
+if version_info[0] == 2:
+    # byte is a one character string in Py2
+    byte2int = ord
+else:
+    byte2int = int
 
 def bytes2int(bytes):
     result = 0
