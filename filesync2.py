ebf22724603f40c0905e51f274599ba1a71b2a08
Vasily Efimov <laer.18@gmail.com>
2019-08-11 20:05:00 +0300

    fs2: move `Hint` to `widgets`
    Signed-off-by: Vasily Efimov <laer.18@gmail.com>

--- filesync2.py
+++ filesync2.py
@@ -20,7 +20,6 @@ from six.moves.tkinter import (
     DISABLED,
     END,
     Text,
-    Toplevel,
     IntVar,
     Menu,
     Label,
@@ -95,6 +94,9 @@ from filemodel import (
     directory,
     file,
     FileInfo_infos
+)
+from widgets import (
+    Hint
 )
 
 
@@ -377,44 +379,6 @@ COLOR_NODE_INCONSISTENT = "#ffdd93"
 
 SETTINGS_FILE = ".fs2.dat"
 
-HINT_HIDE_DELAY = 200 # ms
-
-
-class Hint(Toplevel):
-
-    def __init__(self, *a, **kw):
-        self.x, self.y = kw.pop("x", 0), kw.pop("y", 0)
-
-        Toplevel.__init__(self, *a, **kw)
-        self.overrideredirect(True)
-
-        self._hiding = None
-        self.bind("<Enter>", self._on_enter, "+")
-        self.bind("<Leave>", self._on_leave, "+")
-
-        # After all layout management is done...
-        self.after(10, self._update_position)
-
-    def _update_position(self):
-        self.geometry("%dx%d+%d+%d" % (
-            self.winfo_width(), self.winfo_height(),
-            self.x, self.y
-        ))
-
-    def _on_leave(self, _):
-        self._hide_cancel()
-        self._hiding = self.after(HINT_HIDE_DELAY, self._hide)
-
-    def _on_enter(self, _):
-        self._hide_cancel()
-
-    def _hide_cancel(self):
-        if self._hiding is not None:
-            self.after_cancel(self._hiding)
-            self._hiding = None
-
-    def _hide(self):
-        self.destroy()
 
 if __name__ == "__main__":
     ap = ArgumentParser()
--- widgets/__init__.py
+++ widgets/__init__.py
@@ -1,1 +1,2 @@
 from .coview import *
+from .hint import *
