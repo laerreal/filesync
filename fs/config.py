2670a9e3654ae72e9713be8f7a1adf22dc372e63
Vasily Efimov <laer.18@gmail.com>
2020-03-28 22:00:43 +0300

    fs: user config
    Signed-off-by: Vasily Efimov <laer.18@gmail.com>

---
+++ fs/config.py
@@ -0,0 +1,27 @@
+from os.path import (
+    join,
+    expanduser,
+)
+from .model import *
+
+
+class Config(object):
+
+    __slots__ = (
+        "fs",
+        "port",
+    )
+
+    def __init__(self, **cfg):
+        for k, v in cfg.items():
+            setattr(self, k, v)
+
+CFG_FILE_NAME = join(expanduser("~"), ".filesync.cfg.py")
+
+def load_config():
+    with open(CFG_FILE_NAME, "r") as f:
+        cfg_code = f.read()
+    glob = dict(globals())
+    exec(cfg_code, glob)
+    cfg = glob["cfg"]
+    return cfg
