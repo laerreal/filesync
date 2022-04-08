fe1207b54edfb945d49708db143774e30ebf1f6c
Vasily Efimov <real@ispras.ru>
2022-04-08 20:35:56 +0300

    fs.model.fs_path: rework and add doctest
    Signed-off-by: Vasily Efimov <real@ispras.ru>

--- fs/model.py
+++ fs/model.py
@@ -14,9 +14,21 @@ from six import (
 )
 
 
-re_path_sep = compile(r"[/\\]")
+re_path_sep = compile("\\r\\n|\\n|\\r")
+re_split = re_path_sep.split
 
 def fs_path(string):
+    """
+>>> fs_path("windows\\r\\nmultiline")
+('windows', 'multiline')
+>>> fs_path("linux\\nmultiline")
+('linux', 'multiline')
+>>> fs_path("mac\\nmultiline")
+('mac', 'multiline')
+>>> fs_path("mixed\\rmulti\\nOS\\r\\nmultiline")
+('mixed', 'multi', 'OS', 'multiline')
+    """
+
     if isinstance(string, tuple):
         ret = tuple()
         for e in string:
@@ -24,8 +36,9 @@ def fs_path(string):
     else:
         if isinstance(string, binary_type):
             string = string.decode("utf-8")
-        ret = tuple(re_path_sep.split(string))
+        ret = tuple(re_split(string))
     return ret
+
 
 class AccessRule(object):
 
